564c33127e3f301b5b4b254725c749d7b8e98881
magicenvision <rjrose2003@gmail.com>
2024-12-06 08:56:33 -0500

    Completed getAppSection

--- my-app/components/Features.tsx
+++ my-app/components/Features.tsx
@@ -4,13 +4,16 @@ import React from 'react'
 
 interface FeatureItemProps {
   title: string;
+  icon: string;
+  variant: string;
+  description: string
 }
 
 
 const Features = () => {
   return (
-<section className='border-2 border-red-500 flexCenter flex-col overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
-  <div className='max-container padding-container relative w-full justify-end'>
+<section className=' flexCenter flex-col overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
+  <div className='max-container padding-container relative w-full flex justify-end'>
     <div className='flex flex-1 lg:min-h-[900px]'>
       <Image 
       src="/phone.png"
@@ -30,10 +33,14 @@ const Features = () => {
         />
         <h2 className='bold-40 lg:bold-64'>Our Features</h2>
       </div>
-      <ul className=''>
+      <ul className='mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20'>
         {FEATURES.map((feature) => (
         <FeatureItem 
-        title={feature.title}/>
+        title={feature.title}
+        key={feature.title}
+        icon={feature.icon}
+        variant={feature.variant}
+        description={feature.description}/>
        ))}
       </ul>
     </div>
@@ -41,11 +48,26 @@ const Features = () => {
 </section>  )
 }
 
-const FeatureItem = ({title}: FeatureItemProps) => {
+const FeatureItem = ({title, icon, variant ,description}: FeatureItemProps) => {
   return (
-    <div>
-      {title}
-    </div>
+    <li className='flex w-full flex-1 flex-col items-start'>
+      <div className='rounded-full p-4 lg:p-7 bg-green-50'>
+        <Image 
+        src={icon}
+        alt="map"
+        width={28}
+        height={28}
+        />
+      </div>
+      <h2 className='bold-20 lg:bold-32 mt-5 capatalize'>
+        {title}
+      </h2>
+      <p className='regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none '>
+        {description}
+      </p>
+
+
+    </li>
   )
 
 }
